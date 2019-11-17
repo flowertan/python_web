e3e571979aef3a63e822680f1bd0cc1589a7ca0e
flower <tanchunhua90@hotmail.com>
2019-11-17 10:02:07 +0800

    django学习小组之部署

--- day01/mysite/mysite/settings.py
+++ day01/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^g_(%)&h!1k%t(qnubkhs8@gne@jhe6xrx$h0&bbh1o+hs(#dm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flowertan.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
